02c32a19f214bcad6895d42af2d8368e63eb45d9
ikoch <ihor.kochetov@gmail.com>
2026-03-16 20:22:31 +0100

    Add H1 and H4 heading styles to body editor and CSS

--- sanity/schemas/project.ts
+++ sanity/schemas/project.ts
@@ -67,8 +67,10 @@ export default defineType({
           type: 'block',
           styles: [
             { title: 'Normal', value: 'normal' },
+            { title: 'H1', value: 'h1' },
             { title: 'H2', value: 'h2' },
             { title: 'H3', value: 'h3' },
+            { title: 'H4', value: 'h4' },
             { title: 'Quote', value: 'blockquote' },
           ],
           marks: {
